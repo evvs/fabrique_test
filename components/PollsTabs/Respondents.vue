8d2775c025fe5f17e6847475406d2d9de726cfce
evvs <va.evgeny@gmail.com>
2020-12-23 06:26:46 +0300

    styled  header

--- components/PollsTabs/Respondents.vue
+++ components/PollsTabs/Respondents.vue
@@ -1,9 +1,22 @@
 <template>
-  <h1>Respondents</h1>
+  <div class="res-container">
+    <p class="info">Добавить опрос</p>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.res-container {
+  overflow-y: auto;
+  .info {
+    padding-top: 20px;
+    padding-left: 20px;
+    background-color: #fcfcfc;
+    color: #c5c5c5;
+    font-weight: bold;
+  }
+}
+</style>
